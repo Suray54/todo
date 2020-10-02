77935209b678cc9a7480ca0ac744729da1755690
Suranjan Rai <Suranjanthulungrai@gmail.com>
2020-10-02 17:48:05 +0545

    Updates

---
+++ precache-manifest.f354c426f6dd46c8c7c4657f42e73b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1239d67c3686f158edc25b8609b7962",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "ca558e56033027175fa0",
+    "url": "/todo/static/css/main.ae4d9b8c.chunk.css"
+  },
+  {
+    "revision": "07062036a571d4862d3b",
+    "url": "/todo/static/js/2.09e3cf6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.09e3cf6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca558e56033027175fa0",
+    "url": "/todo/static/js/main.11883c2f.chunk.js"
+  },
+  {
+    "revision": "d69d7058fd40478ea184",
+    "url": "/todo/static/js/runtime-main.11e19a83.js"
+  }
+]);
